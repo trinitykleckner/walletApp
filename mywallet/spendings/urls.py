573dd1bc63d330c272c0642713b9957bd24855ff
Trinity Kleckner <tkleckner@haverford.edu>
2022-10-16 10:37:49 -0400

    added purchase url

--- mywallet/spendings/urls.py
+++ mywallet/spendings/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('NewPurchase.html',views.purchase, name='purchase'),
     path('<int:budget_id>/',views.detail, name='detail'),
     path('WalletData.html', views.wallet, name='wallet'),
     path('index.html', views.index, name='index')
